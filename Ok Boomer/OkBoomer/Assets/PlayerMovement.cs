bfea3f5cb8811165c81e25030acea99be4dae9d4
AYogore <AYogore@users.noreply.github.com>
2020-03-07 15:02:22 -0600

    movement

---
+++ Ok Boomer/OkBoomer/Assets/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveHorizontal;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask IsGround;
+
+    private int extraJumps;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, IsGround);
+
+        moveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+
+        if(facingRight == false && moveHorizontal > 0)
+        { Flip();  }
+        else if(facingRight == true && moveHorizontal < 0)
+        { Flip();  }
+ 
+    }
+
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip() //flips direction and character model
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+
+        transform.localScale = Scaler;
+
+    }
+}
