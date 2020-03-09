454d0ef3234d603ec646e8fbf76f847fc26e7341
AYogore <AYogore@users.noreply.github.com>
2020-03-09 14:46:36 -0500

    Coin Pickup with Score

---
+++ Ok Boomer/OkBoomer/Assets/PickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PickUp : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void Update()
+    {
+        Debug.Log(score);
+        Display();
+    }
+    private void OnTriggerEnter2D(Collider2D pickup)
+    {
+        if (pickup.tag == "PickUp")
+        {
+            score++;
+        }
+    }
+
+    public void Display()
+    {
+        scoreText.text = $"Score = {score}";
+    }
+}
