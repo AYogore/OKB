48119302d58dea30d3fae38cd7035f0a0043d2fe
AYogore <AYogore@users.noreply.github.com>
2020-03-09 13:58:11 -0500

    CleanUP and WinUI

---
+++ Ok Boomer/OkBoomer/Assets/FinishLine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameObject winText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0f;
+        winText.SetActive(true);
+    }
+}
