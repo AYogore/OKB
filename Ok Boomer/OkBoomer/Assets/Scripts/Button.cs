7f762af14f0dd3d571b51192650e8b2adba2a15a
AYogore <AYogore@users.noreply.github.com>
2020-04-18 22:11:02 -0500

    main menu

---
+++ Ok Boomer/OkBoomer/Assets/Scripts/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+
+    public GameObject credits;
+    public GameObject mainMenu;
+    public void StartGame()
+    {  
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void Return()
+    {
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+    }
+}
---
+++ Ok Boomer/OkBoomer/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int health = 1;
+    public float speed;
+    public GameObject deathEffect;
+
+    
+    public int numOFHearts;
+
+    public Image[] hearts;
+    public Sprite full;
+    public Sprite empty;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = full;
+            }
+            else
+            {
+                hearts[i].sprite = empty;
+            }
+            if (i < numOFHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
