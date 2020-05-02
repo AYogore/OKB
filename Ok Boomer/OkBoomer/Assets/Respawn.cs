f3292c5a6cd405be8823a2b64737ba6aa3ee62ac
AYogore <AYogore@users.noreply.github.com>
2020-05-02 17:08:50 -0500

    Respawn mechanic

---
+++ Ok Boomer/OkBoomer/Assets/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject respawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Player player = coll.GetComponent<Player>();
+        if (player.tag == "Player")
+        {
+            player.TakeDamage(1);
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
